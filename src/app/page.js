6783a1798269cd42ab3ee3452c382461aa3c37ad
Rutu Patel <patelrutvij2801@gmail.com>
2024-01-15 17:53:30 -0800

    Preloader + Landing

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
+'use client';
 import styles from './page.module.css'
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Preloader from '../components/Preloader';
+import Landing from '../components/Landing';
+
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 4000)
+      }
+    )()
+  }, [])
+
   return (
     <main className={styles.main}>
-      
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Landing />
     </main>
   )
 }
